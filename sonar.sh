192da6dfd575c709e18cdcfc572a403b5f726d81
bhanuprakash257 <140413678+bhanuprakash257@users.noreply.github.com>
2024-12-05 16:31:51 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -5,7 +5,6 @@ wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.508
 unzip sonarqube-8.9.6.50800.zip
 amazon-linux-extras install java-openjdk11 -y
 useradd sonar
-passwd sonar
 chown sonar:sonar sonarqube-8.9.6.50800 -R
 chmod 777 sonarqube-8.9.6.50800 -R
 su - sonar
