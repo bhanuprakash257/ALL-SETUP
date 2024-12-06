6e7ed99fa9d7f95e2f19a136420de1edc08a785d
bhanuprakash257 <140413678+bhanuprakash257@users.noreply.github.com>
2024-12-06 11:34:58 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -8,7 +8,8 @@ useradd sonar
 chown sonar:sonar sonarqube-8.9.6.50800 -R
 chmod 777 sonarqube-8.9.6.50800 -R
 su - sonar
-
-#run this on server manually
-#sh /opt/sonarqube-8.9.6.50800/bin/linux-x86-64/sonar.sh start
+cd /opt
+cd sonarqube-8.9.6.50800/bin/linux/
+./sonar.sh start
+./sonar status
 #echo "user=admin & password=admin"
